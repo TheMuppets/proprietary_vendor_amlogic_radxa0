d188acaf80278390b80d89fb352104b39b6a26e7
Nolen Johnson <johnsonnolen@gmail.com>
2024-03-15 17:24:01 -0400

    Add radxa0_car to the guards

--- firmware.mk
+++ firmware.mk
@@ -1,6 +1,6 @@
 LOCAL_PATH := $(call my-dir)
 
-ifneq ($(filter radxa0 radxa0_tab,$(TARGET_DEVICE)),)
+ifneq ($(filter radxa0 radxa0_car radxa0_tab,$(TARGET_DEVICE)),)
 
 RADIO_FILES := $(wildcard $(LOCAL_PATH)/radio/*)
 $(foreach f, $(notdir $(RADIO_FILES)), \
